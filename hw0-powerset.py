5a6fb7877bd355637a3fe038a0d998c0d58ee1a7
brandonschein <brandonschein11@gmail.com>
2021-09-17 19:24:05 -0400

    Update hw0-powerset.py

--- hw0-powerset.py
+++ hw0-powerset.py
@@ -14,6 +14,8 @@ element_arr = []
 for i in range (1, len(strings) + 1):
     for element in combinations(strings, i):
         element_arr.append(element)
-
+if(len(element_arr) != 1):
+    element_arr.append("")
+   
 for x in range (0, len(element_arr)):
     print(" ".join(element_arr[x]))
