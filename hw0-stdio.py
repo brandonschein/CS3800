bbe621de409ed8703e75fb85806d756ecb7e708f
brandonschein <brandonschein11@gmail.com>
2021-09-17 20:05:19 -0400

    Update hw0-stdio.py

--- hw0-stdio.py
+++ hw0-stdio.py
@@ -1,8 +1,11 @@
+# using this try except block to deal with empty input case/invalid input
 try:
     string = input()
 except EOFError:
     string = ""
 except:
     print("invalid input")
+
+#prints the string 3 times
 for i in range(3):
     print(string)
