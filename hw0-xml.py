374084d727dbd54dbb87e0d3d237ce5c3ed91e01
brandonschein <brandonschein11@gmail.com>
2021-09-17 20:44:04 -0400

    Update hw0-xml.py

--- hw0-xml.py
+++ hw0-xml.py
@@ -1,5 +1,12 @@
 import xml.etree.cElementTree as ET
 
+def find_state_child(root):
+    for child in root:
+        if(child.tag == "automaton"):
+            return(child)
+        else:
+            find_state_child(child) 
+
 xmlfile = input()
 
 state_names = []
@@ -9,6 +16,9 @@ accept_states = []
 with open(xmlfile, "r") as file:
     tree = ET.parse(file)
     root = tree.getroot()
+ 
+    if(root.tag != "automaton"):
+        root = find_state_child(root)          
 
     for child in root:
         if(child.tag == "state"):
@@ -18,20 +28,9 @@ with open(xmlfile, "r") as file:
                 if(ch.tag == "final"):
                     accept_states.append(child.attrib['name'])
             state_names.append(child.attrib['name'])
+    
 
     
     print(" ".join(state_names))
     print(start_state)
     print(" ".join(accept_states))
-
-    
-
-        
-    #for child in root:
-    #    if(child.tag == "state"):
-    #        state_names.append(child.attrib[1])
-    
-    #print(state_names)
-
-#for state in root.iter('state')
-#   state_names.append(state.attrib[1])
