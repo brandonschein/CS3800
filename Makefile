73460cc56a209e876eb8223ccab61bb873152a46
brandonschein <brandonschein11@gmail.com>
2021-09-24 13:06:28 -0400

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,5 @@
+#setup:
+#	apt-get install -y python3
+
+run-hw1-dfa:
+	python3 hw1-dfa.py 
---
+++ hw1-dfa.py
@@ -0,0 +1,43 @@
+# how DFA's will be represented in my 
+class DFA:
+    def __init__(self, states, alphabet, transitions, start, accepts):
+        self.states = states
+        self.alphabet = alphabet
+        self.transitions = transitions 
+        self.start = start
+        self.accpets = accepts
+
+#get the input 
+try:
+    to_ret = input()
+except EOFError:
+    print("Invalid input")
+
+#sets up the elements used to create a DFA
+states = ["q1", "q2"]
+alphabet = ["0", "1"]
+delta = [["q1", "0", "q1"], ["q1", "1", "q2"], ["q2", "0", "q2"], ["q2", "1", "q1"]]
+start_state = "q1"
+accept_states = ["q1"]
+
+#creates the DFA to use as an example in testing 
+testDFA = DFA(states, alphabet, delta, start_state, accept_states)
+
+# determing what to do based off of the input given 
+if(to_ret == "states"):
+    for i in testDFA.states:
+        print(i)
+elif(to_ret == "alpha"):
+    for i in testDFA.alphabet:
+        print(i)
+elif(to_ret == "transitions"):
+    for x in testDFA.transitions:
+        print(" ".join(x))
+elif(to_ret == "start"):
+    print(testDFA.start)
+elif(to_ret == "accepts"):
+    for i in testDFA.accpets:
+        print(i)
+        
+else:
+    print("Invalid Input")
