583c4c00d158838483e24d50d683e613bbf0d2ca
brandonschein <brandonschein11@gmail.com>
2021-11-05 13:40:06 -0400

    Update hw6-dyck.py

--- hw6-dyck.py
+++ hw6-dyck.py
@@ -1,4 +1,5 @@
 import xml.etree.cElementTree as ET
+import sys
 
 #CFG definition 
 
@@ -11,23 +12,47 @@ class CFG:
 
 def CFGtoXML(cfg):
     root = ET.Element("structure")
+    ET.SubElement(root, "type").text = "grammar"
 
-    ET.SubElement(root, "type").text = "grammer"
+    # load the start terminal first
+    rights = cfg.rules.get(cfg.start)
+    if(rights != None):
+        for right_list in rights:
+            right_str = ""
+            for right in right_list:
+                right_str += right
+            cur_production = ET.SubElement(root, "production")
 
-    for i in cfg.rules:
-        cur_production = ET.SubElement(root, "production")
+            
+            ET.SubElement(cur_production, "left").text = cfg.start
+            ET.SubElement(cur_production, "right").text = right_str
+    
+    for variable in cfg.variables:
+        if (variable != cfg.start):
+            rights = cfg.rules.get(variable)
+            if (rights != None):
+                for right_list in rights:
+                    right_str = ""
+                    for right in right_list:
+                        right_str += right
+                    cur_production = ET.SubElement(root, "production")
 
-        ET.SubElement(cur_production, "left").text = i[0]
-        ET.SubElement(cur_production, "right").text = i[1]
-
+                    ET.SubElement(cur_production, "left").text = cfg.start
+                    ET.SubElement(cur_production, "right").text = right_str
+    
     tree = ET.ElementTree(root)
-    tree.write("test.xml")
+    tree.write(sys.stdout, encoding="unicode")
         
 
 # making the CFG for language L (L = {w | w = is the string of well-balanced matching brackets})
 cfg_variables = ["S"]
-cfg_terminals = ['(', ')', '[', ']']
-cfg_rules = [("S", "[S]"), ("S", "(S)"), ("S", "SS"), ("S", "")]
+cfg_terminals = ["(", ")", "[", "]"]
+cfg_rules = {
+    "S" : [["[", "S", "]"],
+        ["(", "S", ")"],
+        ["S", "S"],
+        []]
+}
 cfg_start = "S"
 
 
