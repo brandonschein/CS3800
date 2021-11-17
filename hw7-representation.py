73d19a4a2a40a7528c583fd6222d8415d3a4fe5f
brandonschein <brandonschein11@gmail.com>
2021-11-17 07:59:28 -0500

    Update hw7-representation.py

--- hw7-representation.py
+++ hw7-representation.py
@@ -1,38 +1,48 @@
-#class for PDAs, refer to read me for definition
+import sys
+import xml.etree.cElementTree as ET
+
 class PDA:
-    def __init__(self, states, inp_alpha, stack_alpha, transitions, start, accepts):
-       self.states = variables
-       self.inp_alphabet = inp_alphabet
-       self.stack_alphabet = stack_alphabet
-       self.transitions = transitions
-       self.start = start
-       self.accept = accept
+    def __init__(self, states, alphabet, stack_alphabet, transitions, start, accepts):
+        self.states = states
+        self.alphabet = alphabet
+        self.stack_alphabet = stack_alphabet
+        self.transitions = transitions
+        self.start = start
+        self.accepts = accepts
 
-    
-    # takes a CFG and prints it out to stdout
 def PDAtoXML(pda):
-    root = ET.Element("automaton")
-
-    swap_arr = []
-    for i in range(0, len(xmlDFA.states)):
-        cur_state = ET.SubElement(root, "state", id=str(i), name=xmlDFA.states[i])
-        swap_arr.append(str(i))
-        swap_arr.append(xmlDFA.states[i])
-
-        if(xmlDFA.states[i] == xmlDFA.start):
+    root = ET.Element("structure")
+    ET.SubElement(root, "type").text = "pda"
+    automaton = ET.SubElement(root, "automaton")
+    for state in range(0, len(pda.states) - 1):
+        print(state)
+        cur_state = ET.SubElement(automaton, "state", id=str(state), name="q" + str(state))
+        if pda.states[state] == pda.start:
             ET.SubElement(cur_state, "initial")
-        if(xmlDFA.states[i] in xmlDFA.accepts):
+        if pda.states[state] in pda.accepts:
             ET.SubElement(cur_state, "final")
 
-    for i in xmlDFA.transitions:
-        cur_transition = ET.SubElement(root, "transition")
-        ET.SubElement(cur_transition, "from").text = swap_arr[swap_arr.index(i[0]) - 1]
-        ET.SubElement(cur_transition, "to").text = swap_arr[swap_arr.index(i[2]) - 1]
-        ET.SubElement(cur_transition, "read").text = i[1]
-
+    for key in pda.transitions:
+        right_side = pda.transitions[key]
+        print(right_side)
+        transition = ET.SubElement(automaton, "transition")
+        for list in right_side:
+            ET.SubElement(transition, "from").text = str(key)
+            ET.SubElement(transition, "to").text = str(list[0])
+            ET.SubElement(transition, "read").text = str(list[1])
+            ET.SubElement(transition, "push").text = str(list[2])
+            ET.SubElement(transition, "pop").text = str(list[3])
+    
     tree = ET.ElementTree(root)
-    tree.write(sys.stdout, encoding="unicode")
+    tree.write(sys.stdout, encoding="unicode") 
 
-myPDA = PDA(['a','b'],['l','m'],['x','y'],{},'a',['a','b'])
+pdaw_states = ['q0', 'q1', 'q2']
+pdaw_alphabet = ['[', ']', '(', ')']
+pdaw_stack_alphabet = ['[', ']', '(', ')']
+pdaw_transitions = {0: [[1, None, '$', None]], 1: [[1, '[', '[', None], [1, '(', '(', None], [1, ']', None, ']'], [1, ')', None, ')'], [2, None, None, "$"]]}
+pdaw_start = 'q0'
+pdaw_accepts = 'q2'
 
-PDAtoXML(myPDA)
+pdaw = PDA(pdaw_states, pdaw_alphabet, pdaw_stack_alphabet, pdaw_transitions, pdaw_start, pdaw_accepts)
+
+PDAtoXML(pdaw)
