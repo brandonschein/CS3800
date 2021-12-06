5852462a2c1a41bf462f2bf40ba455559aeb2405
brandonschein <brandonschein11@gmail.com>
2021-12-06 16:38:15 -0500

    Update hw10-matrix.py

--- hw10-matrix.py
+++ hw10-matrix.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # # Program to multiply two matrices using nested loops
 
-# # 3x3 matrix
+# # 4x2 matrix
 # X = [[1, 2],
 #     [1, 3],
 #     [6, 2],
 #     [5, 1]]
-# # 3x4 matrix
+# # 2x3 matrix
 # Y = [[3, 6, 1],
 #     [-2, 3, 1]]
 # # result is 3x4
@@ -35,16 +35,16 @@ Z = [] # n by k
 result = []
 # n m k 
 def make_matricies(input_arr):
-    n, m, k = input_arr[0].split(" ")
+    n, m, k = input_arr[0].split()
     curr = 1
     for i in range(curr, curr + int(n)):
-        X.append(list (map(int, input_arr[i].split(" "))))
+        X.append(list (map(int, input_arr[i].split())))
     curr += int(n)
     for i in range(curr, curr + int(m)):
-        Y.append(list (map(int, input_arr[i].split(" "))))
+        Y.append(list (map(int, input_arr[i].split())))
     curr += int(m)
     for i in range(curr, curr + int(n)):
-        Z.append(list (map(int, input_arr[i].split(" "))))
+        Z.append(list (map(int, input_arr[i].split())))
     empty_row_matrix = []
     for i in range(int(k)):
         empty_row_matrix.append(0)
@@ -53,11 +53,11 @@ def make_matricies(input_arr):
     
 
 def calculate_result():
-    for i in range(len(X)):
+    for i in range(len(X)): #4 n 
    # iterate through columns of Y
-        for j in range(len(Y[0])):
+        for j in range(len(Y[0])): #3 k 
             # iterate through rows of Y
-            for k in range(len(Y)):
+            for k in range(len(Y)): #2 m 
                 result[i][j] += int(X[i][k]) * int(Y[k][j])
 
 
@@ -68,10 +68,10 @@ input_arr[len(input_arr) - 1] = input_arr[len(input_arr) - 1].split("\n")[0]
 #print(input_arr)
 
 make_matricies(input_arr)
-#print(X)
-#print(Y)
-#print(Z)
-#print(result)
+# print(X)
+# print(Y)
+# print(Z)
+# print(result)
 
 calculate_result()
 #print(result)
